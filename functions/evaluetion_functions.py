969e322adc995387abf779b29c37712b98e16704
bobboyms <$bob250387>
2022-09-17 14:13:11 -0300

    Ajustes nas avaliações

--- functions/evaluetion_functions.py
+++ functions/evaluetion_functions.py
@@ -93,3 +93,129 @@ def merge(nir_ndcg, els_ndcg):
         'nir_ndcg_k': nir_ndcg_k,
         'els_ndcg_k': els_ndcg_k,
     }
+
+
+def get_query_documents(query_id, documents, engine):
+    temp_documents = []
+    for doc in documents:
+        if str(query_id) != doc['query_id']:
+            continue
+        query_results = {}
+        if engine == "NIR":
+            query_results = doc['result']['queryResults']
+        if engine == "ELS":
+            query_results = documents[0]['result']['hits']['hits']
+
+        for result in query_results:
+            temp_documents.append(result)
+    return temp_documents
+
+
+def is_contain_document(doc_id, qrels):
+    for qrel in qrels:
+        if (qrel.doc_id == str(doc_id)):
+            return True
+    return False
+
+
+def get_qrels(query_id, dataset):
+    temp_qrels = []
+    for qrel in dataset.qrels_iter():
+        if qrel.query_id == str(query_id):
+            temp_qrels.append(qrel)
+    return temp_qrels
+
+
+def get_total_retrieved_relevant(query_id, documents, dataset, engine):
+    count = 0
+    qrels = get_qrels(query_id, dataset)
+    query_documents = get_query_documents(query_id, documents, engine)
+
+    for doc in query_documents:
+        document_id = {}
+        if engine == "NIR":
+            document_id = doc['document']['id']
+        if engine == "ELS":
+            document_id = doc['_id']
+
+        if is_contain_document(document_id, qrels):
+            count += 1
+
+    return count
+
+
+def get_total_relevant(query_id, dataset):
+    count = 0
+    for qrel in dataset.qrels_iter():
+        if qrel.query_id == str(query_id):
+            count += 1
+    return count
+
+
+# precisão = 15 relevantes recuperados / 25 total recuperado
+def calc_precision(retrieved_relevant, total_retrieved):
+    if retrieved_relevant == 0:
+        return 0
+    return retrieved_relevant / total_retrieved
+
+
+# recall = 15 recuperados relevantes / 20 total relevante
+def calc_recall(retrieved_relevant, total_relevant):
+    if retrieved_relevant == 0:
+        return 0
+    return retrieved_relevant / total_relevant
+
+
+def f_score(recall, precision):
+    n = 2 * (precision * recall)
+    d = precision + recall
+    return n / d
+
+
+def dict_to_list(dict):
+    temp = []
+    for key in dict:
+        temp.append(dict[key])
+    return temp
+
+
+def recall_precision(dataset, documents, engine):
+    query_ids = []
+    total_relevants = []
+    total_retrieveds = []
+    total_retrieved_relevants = []
+    recall = []
+    precision = []
+    f_scores = []
+    for id in range(1, 256):
+
+        total_retrieved_relevant = get_total_retrieved_relevant(
+            id, documents, dataset, engine)
+
+        if total_retrieved_relevant == 0:
+            continue
+
+        query_ids.append(id)
+        total_relevant = get_total_relevant(id, dataset)
+        total_retrieved = len(get_query_documents(id, documents, engine))
+
+        total_relevants.append(total_relevant)
+        total_retrieveds.append(total_retrieved)
+        total_retrieved_relevants.append(total_retrieved_relevant)
+
+        value_recall = calc_recall(total_retrieved_relevant, total_relevant)
+        value_precision = calc_precision(
+            total_retrieved_relevant, total_retrieved)
+        recall.append(value_recall)
+        precision.append(value_precision)
+        f_scores.append(f_score(value_recall, value_precision))
+
+    return {
+        "query_ids": query_ids,
+        "total_relevants": total_relevants,
+        "total_retrieveds": total_retrieveds,
+        "total_retrieved_relevants": total_retrieved_relevants,
+        "recall": recall,
+        "precision": precision,
+        "f_scores": f_scores
+    }
